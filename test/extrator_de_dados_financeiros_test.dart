3f1aa00318c4df8d5ddd95c6198a13dd60319e11
s7thiago <thyagosousasilva@gmail.com>
2020-02-24 11:15:16 -0300

    Criando o projeto

---
+++ test/extrator_de_dados_financeiros_test.dart
@@ -0,0 +1,8 @@
+import 'package:extrator_de_dados_financeiros/extrator_de_dados_financeiros.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
