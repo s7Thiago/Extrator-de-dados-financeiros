38c97c9a9aa37aa7425e4fe5eccf2c7ebee4962a
s7thiago <thyagosousasilva@gmail.com>
2020-02-24 11:17:50 -0300

    Removendo coisas desnecessárias

---
+++ .history/bin/main_20200224111045.dart
@@ -0,0 +1,5 @@
+import 'package:extrator_de_dados_financeiros/extrator_de_dados_financeiros.dart' as extrator_de_dados_financeiros;
+
+void main(List<String> arguments) {
+  print('Hello world: ${extrator_de_dados_financeiros.calculate()}!');
+}
--- test/extrator_de_dados_financeiros_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:extrator_de_dados_financeiros/extrator_de_dados_financeiros.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
